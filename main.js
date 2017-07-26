8f1a6482ca9d483d4c707cedd2375d603a449f92
Gopard <w8it@mail.ru>
2017-07-26 22:43:41 +0300

    added track-list

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600,
+    icon: path.join(__dirname, 'icon/icon.ico')})
+
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +20,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+ // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
